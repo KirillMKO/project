ed2cbfcffe61fbfdec992090fd61345ba385950d
Kirill <exaple@gmail.com>
2022-09-11 20:07:09 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "");
+// console.log(answer);
+
+// const answers = [];
+// answers[0] = prompt("Как Ваше имя?", "");
+// answers[1] = prompt("Как Ваше фамилия?", "");
+// answers[2] = prompt("Сколько Вам лет?", "");
+
+// document.write(answers);
+
+// const user = 'Ivan';
+
+// console.log(`Привет, ${user}`);
+
+let incr = 10,
+  decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
